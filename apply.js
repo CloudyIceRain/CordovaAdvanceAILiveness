dd9eb83ead4f0e9bb89cd50b177cffa48223bdba
cloudy2018 <zhj161@163.com>
2020-12-15 19:18:42 +0800

    添加脚本文件apply.js，并且添加依赖。

---
+++ apply.js
@@ -0,0 +1,45 @@
+var BUILD_GRADLE_PATH = "./platforms/android/app/build.gradle";
+
+var deferral, fs, path;
+
+function log(message) {
+    console.log(PLUGIN_NAME + ": " + message);
+}
+
+function onFatalException(ex){
+    log("EXCEPTION: " + ex.toString());
+    deferral.resolve(); // resolve instead of reject so build doesn't fail
+}
+
+function run() {
+  try {
+      fs = require('fs');
+      // path = require('path');
+  } catch (e) {
+      throw("Failed to load dependencies: " + e.toString());
+  }
+  var buildGradle = fs.readFileSync(BUILD_GRADLE_PATH).toString(),
+
+  let search_str = "dirs project(':CordovaAdvanceAILiveness:liveness').file('libs')";
+  let str = "repositories {    flatDir {        dirs project(':CordovaAdvanceAILiveness:liveness').file('libs')    }  }"
+  if (buildGradle.indexOf(search_str) == -1){
+    //没有这个字符串
+    log("try add liveness to build gradle.");
+    buildGradle += "\n" + str + "\n";//末尾添加
+    fs.writeFileSync(BUILD_GRADLE_PATH, buildGradle, 'utf8');
+  }
+  ////运行结束
+  deferral.resolve();
+}
+
+module.exports = function (ctx) {
+    try{
+        deferral = require('q').defer();
+    }catch(e){
+        e.message = 'Unable to load node module dependency \'q\': '+e.message;
+        onFatalException(e);
+        throw e;
+    }
+    attempt(run)();
+    return deferral.promise;
+};
